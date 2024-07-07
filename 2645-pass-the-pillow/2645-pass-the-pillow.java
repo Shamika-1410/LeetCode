c807cbf52865e5f2771ec1da22da191e745e53c7
Shamika Aney <122042949+Shamika-1410@users.noreply.github.com>
2024-07-07 18:17:44 +0530

    Time: 0 ms (100%), Space: 40.5 MB (24.32%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.java
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -1,11 +1,11 @@
 class Solution {
     public int passThePillow(int n, int time) {
-        int rounds = time / (n - 1);
-        int etime = time % (n - 1);
-        if (rounds % 2 == 0) {
-            return etime + 1; 
-        } else {
-            return n - etime;
+       if(time<n) return time+1;
+       int rounds = time/(n-1);
+       int turn = time%(n-1);
+        if(rounds%2==0){
+            return 1+turn;
         }
+        return n-turn;
     }
 }
