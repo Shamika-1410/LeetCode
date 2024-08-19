01294ad56a1d81070c5117403df0e9197a1e151b
Shamika Aney <122042949+Shamika-1410@users.noreply.github.com>
2024-08-19 16:42:35 +0530

    Time: 1 ms (100%), Space: 45.9 MB (63.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,26 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int cnt=0;
-        int k=0;
-        Set<Integer> set = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0) cnt++;
-            if(nums[i]==0 || set.contains(nums[i])){
-                int index = nextNonZero(i,nums);
-                if(index!=i && index<nums.length){
-                    nums[i]=nums[index];
-                    nums[index]=0;
-                    k++;
-                    set.add(nums[index]);
-                }
+        int a=0;
+        for(int i : nums){
+            if(i!=0){
+                nums[a]=i;
+                a++;
             }
         }
-    }
-    public int nextNonZero(int k, int nums[]){
-        int i=k;
-        for(i=k;i<nums.length;i++){
-            if(nums[i]!=0) return i;
+        for(int i = a;i<nums.length;i++){
+            nums[i]=0;
         }
-        return i;
     }
 }
