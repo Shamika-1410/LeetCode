9eb5d877254e7ea1ca50ed194d58bb4b4385452e
Shamika Aney <122042949+Shamika-1410@users.noreply.github.com>
2024-08-19 18:08:45 +0530

    Time: 2 ms (98.13%), Space: 45.3 MB (5.04%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,14 @@
+class Solution{
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int a = nums[i];
+            if(map.containsKey(target - a)){
+                return new int[] { i , map.get(target - a)};
+            }else{
+                map.put(a,i);
+            }
+        }
+        return new int[] {};
+    }
+}
