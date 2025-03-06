b4c92f80b85078873861495704dfd2107cab3c38
Shamika Aney <122042949+Shamika-1410@users.noreply.github.com>
2025-03-06 12:20:33 +0530

    Time: 5 ms (44.38%), Space: 45.5 MB (62.19%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -1,8 +1,11 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        Set<Integer> set = new TreeSet<>();
+        Set<Integer> set = new HashSet<>();
         int ans[] = new int[2];
         int n = grid.length;
+        int N = n*n;
+        int totalSum = N*(N+1)/2;
+        int sum=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 int a = grid[i][j];
@@ -11,17 +14,11 @@ class Solution {
                 }else{
                     set.add(a);
                 }
+                sum+=a;
             }
         }
-        int prev = 0;
-        for(Integer a : set){
-            int next = a;
-            if(next-prev!=1){
-                ans[1]=next-1;
-            }
-            prev = next;
-        }
-        if(ans[1]==0)ans[1]=n*n;
+        sum-=ans[0];
+        ans[1]=totalSum-sum;
         return ans;
     }
 }
