d26051360424ca0149a22f2d0350be0dececcadb
Shamika Aney <122042949+Shamika-1410@users.noreply.github.com>
2025-08-01 14:17:55 +0530

    Time: 1 ms (82.66%), Space: 42.1 MB (39.47%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,34 +1,27 @@
-import java.util.*;
-
 class Solution {
-    public static List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
-
-        if (numRows == 0) {
-            return triangle;
-        }
-
-        for (int i = 0; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>();
-
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
-                    row.add(1);
-                } else {
-                    List<Integer> prevRow = triangle.get(i - 1);
-                    int val = prevRow.get(j - 1) + prevRow.get(j);
-                    row.add(val);
-                }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        ans.add(list1);
+        if(numRows==1)return ans;
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(1);
+        ans.add(list2);
+        if(numRows==2)return ans;
+        for(int i=2;i<numRows;i++){
+            List<Integer> list3 = new ArrayList<>();
+            list3.add(1);
+            List<Integer> prevList = ans.get(i-1);
+            for(int j=1;j<=i-1;j++){
+                int a = prevList.get(j);
+                int b = prevList.get(j-1);
+                list3.add(a+b);
             }
-
-            triangle.add(row);
+            list3.add(1);
+            ans.add(list3);
         }
-        return triangle;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int numRows = sc.nextInt();
-        System.out.println(generate(numRows));
+        return ans;
     }
 }
