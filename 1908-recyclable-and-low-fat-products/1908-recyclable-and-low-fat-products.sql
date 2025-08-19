68cac03f6f50e2416dbcd59e3d541dd67de7fb78
Shamika Aney <122042949+Shamika-1410@users.noreply.github.com>
2025-08-19 13:46:28 +0530

    Time: 599 ms (40.89%), Space: 0B (100%) - LeetHub

--- 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-select product_id from Products where low_fats = 'Y' and recyclable = 'Y';
+/* Write your PL/SQL query statement below */
+SELECT product_id FROM PRODUCTS WHERE low_fats = 'Y' AND recyclable = 'Y';
