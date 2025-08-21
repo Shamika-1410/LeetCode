b8c2bb686c547d7d0765e69adb2a2770c031a612
Shamika Aney <122042949+Shamika-1410@users.noreply.github.com>
2025-08-21 13:16:18 +0530

    Time: 812 ms (50.65%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,4 +1,4 @@
 /* Write your PL/SQL query statement below */
-select p.product_name, s.year, s.price
-from Sales s Inner join Product p
-on s.product_id = p.product_id;
+select Product.product_name, Sales.year, Sales.price
+from Sales left join Product 
+on Sales.product_id = Product.product_id;
