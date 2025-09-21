121e861ec580af712ac0a9e7db9cd55b3a39788a
Shamika Aney <122042949+Shamika-1410@users.noreply.github.com>
2025-09-21 18:25:21 +0530

    Time: 0 ms (100%), Space: 46 MB (36.32%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,14 +1,12 @@
-class Solution{
-    public int maxProfit(int[] prices){
-        int currBuy = prices[0];
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
         int profit = 0;
-        int ans = 0;
-        for(int i=0;i < prices.length;i++){
-            if( prices[i]>currBuy){
-                ans+= prices[i]-currBuy;
-            }
-            currBuy = prices[i];
+        for(int i=1;i<n;i++){
+            if(prices[i] > prices[i-1]){
+                profit += prices[i]-prices[i-1];
+            }  
         }
-        return ans;
+        return profit;
     }
 }
